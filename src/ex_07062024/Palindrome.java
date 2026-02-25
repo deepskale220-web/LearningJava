8172d07bc486fe9c394b08466fd668186e53b57e
deepa <deepskale220@gmail.com>
2026-02-25 15:45:28 +0530

    Merge remote-tracking branch 'origin/master'

--- src/ex_07062024/Palindrome.java
+++ src/ex_07062024/Palindrome.java
@@ -1,4 +1,26 @@
 package ex_07062024;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Palindrome {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the string");
+        String str = sc.nextLine();
+        String reversed="";
+        for (int i=str.length()-1; i>=0 ;i--)
+        {
+            reversed = reversed + str.charAt(i);
+        }
+
+        if( str.equals(reversed))
+        {
+            System.out.println("String is palindrome");
+        }
+        else
+        {
+            System.out.println("String is not a Palindrome");
+        }
+    }
 }
